fada99568f2e3bfecf0f30b47c0e8af5ac21be6a
Tanyo <tnikolov@rnd-solutions.net>
2021-10-01 18:34:35 +0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 
 
 const app = express();
+
+app.use(cors({origin: "http://test"}))
 
 app.get("/", (req, res) => {
     res.json({msg: "Test Message!"})
